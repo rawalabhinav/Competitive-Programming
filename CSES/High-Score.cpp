08237d410d1be5b83f85ea23f582711b879b31c2
rawalabhinav <abhinavrawal2002@gmail.com>
2023-05-29 23:10:00 +0530

    Complete

--- CSES/High-Score.cpp
+++ CSES/High-Score.cpp
@@ -6,44 +6,35 @@ using namespace std;
 #endif
 
 #define int long long int
-
-int n, m;
-vector <vector <int>> adj;
-vector <int> vis, node;
-
-void dfs(int u, int p){
-    if (vis[u]) {
-        for(; p != u; p = vis[p]){
-            node.emplace_back(p);
-        }
-
-        cout << node.size() + 2 << '\n' << u << ' ';
-        for(int& e: node) cout << e << ' ';
-        cout << u; exit(0);
-    }
-
-    vis[u] = p;
-    for(int& v: adj[u]){
-        if (v != vis[u]) dfs(v, u);
-    }
-}
+constexpr int inf = 1e16;
 
 void solve(){
-    cin >> n >> m;
+    int n, m; cin >> n >> m;
+    
+    vector <int> d(n + 1, inf); d[1] = 0;
+    vector <tuple <int, int, int>> edge(m);
 
-    adj.resize(n + 1), vis.resize(n + 1);
-    for(int u, v, i = 0; i < m; ++i){
-        cin >> u >> v;
-
-        adj[u].emplace_back(v);
-        adj[v].emplace_back(u);
+    for(auto& [u, v, w]: edge){
+        cin >> u >> v >> w; w = -w;
     }
 
-    for(int i = 1; i <= n; ++i){
-        if (!vis[i]) dfs(i, i);
+    for(int i = 1; i < n; ++i){
+        for(auto& [u, v, w]: edge){
+            if (d[u] != inf && d[u] + w < d[v]){
+                d[v] = d[u] + w;
+            }
+        }
     }
 
-    cout << "IMPOSSIBLE";
+    for(int i = 1; i < n; ++i){
+        for(auto& [u, v, w]: edge){
+            if (d[u] != inf && d[u] + w < d[v]){
+                d[v] = -inf;
+            }
+        }
+    }
+
+    cout << (d[n] > -inf ? -d[n] : -1);
 }
 
 signed main(){
