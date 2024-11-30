1265c23c7028a0f5941c7f0a6a30cbf9a14f3661
rawalabhinav <abhinavrawal2002@gmail.com>
2024-12-01 04:51:22 +0530

    Fixed pre print error and refactored

--- CSES/Building-Teams.cpp
+++ CSES/Building-Teams.cpp
@@ -5,31 +5,11 @@ using namespace std;
     #include <Collections/debug.h>
 #endif
 
-#define int long long int
-
-int n, m;
-vector <vector <int>> adj;
-vector <int> vis;
-
-void dfs(int u, int team){
-    if (vis[u] == team) return;
-    else if (vis[u]){
-        cout << "IMPOSSIBLE";
-        exit(0);
-    }
-
-    vis[u] = team;
-    team = !(team - 1) + 1;
-
-    for(int& v: adj[u]){
-        dfs(v, team);
-    }
-}
-
 void solve(){
-    cin >> n >> m;
-
-    adj.resize(n + 1), vis.resize(n + 1);
+    int n, m; cin >> n >> m;
+    vector <vector <int>> adj(n + 1);
+    vector <int> vis(n + 1);
+  
     for(int u, v, i = 0; i < m; ++i){
         cin >> u >> v;
 
@@ -37,10 +17,26 @@ void solve(){
         adj[v].emplace_back(u);
     }
 
+    function <void(int, int)> dfs = [&](int u, int team){
+        if (vis[u] == team) return;
+
+        if (vis[u]){
+            cout << "IMPOSSIBLE";
+            exit(0);
+        }
+
+        vis[u] = team;
+        team = !(team - 1) + 1;
+
+        for(int& v: adj[u]) dfs(v, team);    
+    };
+
     for(int i = 1; i <= n; ++i){
         if (!vis[i]) dfs(i, 1);
-        cout << vis[i] << ' ';
     }
+
+    for (int i = 1;  i <= n; ++i)
+        cout << vis[i] << ' ';
 }
 
 signed main(){
